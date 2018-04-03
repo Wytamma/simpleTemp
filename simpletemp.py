024abd08088631979642fc1d6afb333266ef7c3e
Wytamma Wirth <wytamma.wirth@me.com>
2018-04-03 15:39:44 +1000

    probe reading

--- simpletemp.py
+++ simpletemp.py
@@ -1,3 +1,4 @@
+from ds18b20 import all_probes
 import argparse
 from datetime import datetime
 from time import sleep
@@ -15,10 +16,24 @@ args = vars(parser.parse_args())
 url = 'http://{}/'.format(args['database'])
 samping_interval = args['interval']
 
+local_probes_list = [] 
+
 def run():
     r = requests.get(url+'records/example1?limit=1')
     print(r.json())
-
+    for p in all_probes():
+        if p.probe_id not in local_probes_list:
+            r = requests.post(url+'probes', data = {'probe_id':p.probe_id})
+            local_probes_list.append(p.probe_id})
+        temp = p.read_temperature()
+        if temp is None:
+            print('Could not read probe:', p)
+            continue
+        print(datetime.now().isoformat(), p, temp)
+        r = requests.post(url+'records/'+p.probe_id, data = {'temperature':temp, 'time':datetime.now().isoformat()})
+        if r.status_code != requests.codes.ok:
+            print(p, 'Database reuest failed.')
+            
 if __name__ == '__main__':
     while True:
         run()
